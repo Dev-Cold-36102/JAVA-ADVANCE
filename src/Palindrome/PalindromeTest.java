3dae806c0eabbd5b45818a5ecb0f770c4eb46410
Dev.Cold <devcolduet@gmail.com>
2020-01-09 11:25:33 +0700

    add folder

---
+++ src/Palindrome/PalindromeTest.java
@@ -0,0 +1,18 @@
+package Palindrome;
+
+import java.util.Scanner;
+
+public class PalindromeTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String string = "";
+        do {
+            System.out.println("please enter the string:");
+            string = scanner.nextLine();
+        } while (string.equals(String.valueOf("")));
+        int i = 0, j = string.length() - 1;
+        Palindrome palindrome = new Palindrome(string);
+        System.out.println(palindrome.checkPalindrome(i, j));
+    }
+}
